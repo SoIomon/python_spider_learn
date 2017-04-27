547ed729beb5db00114ff03e58838c9c5572762b
SoIomon <770589196@qq.com>
2017-04-27 23:12:12 +0800

    Day_3
    作业半抄半写完成后，加入了爬取帖子内容的方法，并且添加了工具类，以修改内容中夹杂的图片，链接，<ba>部分。

--- baidutieba_00.py
+++ baidutieba_00.py
@@ -3,6 +3,33 @@
 import urllib2
 import re
 
+#处理页面标签类
+class Tool:
+    #去除img标签,7位长空格
+    removeImg = re.compile('<img.*?>| {7}|')
+    #删除超链接标签
+    removeAddr = re.compile('<a.*?>|</a>')
+    #把换行的标签换为\n
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    #将表格制表<td>替换为\t
+    replaceTD= re.compile('<td>')
+    #把段落开头换为\n加空两格
+    replacePara = re.compile('<p.*?>')
+    #将换行符或双换行符替换为\n
+    replaceBR = re.compile('<br><br>|<br>')
+    #将其余标签剔除
+    removeExtraTag = re.compile('<.*?>')
+    def replace(self,x):
+        x = re.sub(self.removeImg,"",x)
+        x = re.sub(self.removeAddr,"",x)
+        x = re.sub(self.replaceLine,"\n",x)
+        x = re.sub(self.replaceTD,"\t",x)
+        x = re.sub(self.replacePara,"\n    ",x)
+        x = re.sub(self.replaceBR,"\n",x)
+        x = re.sub(self.removeExtraTag,"",x)
+        #strip()将前后多余内容删除
+        return x.strip()
+
 #百度贴吧爬虫类
 class BDTB:
 
@@ -10,6 +37,7 @@ class BDTB:
     def __init__(self,baseUrl,seeLZ):
         self.baseURL = baseUrl
         self.seeLZ = '?see_lz=' + str(seeLZ)
+        self.tool = Tool()
 
     #传入页码，获取帖子的代码
     def getPage(self,pageNum):
@@ -46,8 +74,19 @@ class BDTB:
         else:
             return None
 
+    # 获取每一层楼的内容,传入页面内容
+    def getContent(self, page):
+        pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
+        items = re.findall(pattern, page)
+        #for item in items:
+        #    print item
+        print self.tool.replace(items[1])
+        for item in items:
+            print item
+
 baseURL = 'http://tieba.baidu.com/p/3138733512'
 bdtb = BDTB(baseURL,1)
 #bdtb.getPage(1)
 #bdtb.getTitle()
-bdtb.getPageNum()
+#bdtb.getPageNum()
+bdtb.getContent(bdtb.getPage(1))
