0c6e67d94e45513d64fd02ab96c2d14d2c3c3c4a
SoIomon <770589196@qq.com>
2017-04-28 23:59:35 +0800

    Day_4
    小小的复制下，再改进。

--- baidutieba_01.py
+++ baidutieba_01.py
@@ -78,15 +78,55 @@ class BDTB:
     def getContent(self, page):
         pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
         items = re.findall(pattern, page)
-        floor = 1
+        contents = []
         for item in items:
-            print floor, u"楼------------------------------------------------------------------------------------------------------------------------------------\n"
-            print self.tool.replace(item)
-            floor += 1
+            # 将文本进行去除标签处理，同时在前后加入换行符
+            content = "\n" + self.tool.replace(item) + "\n"
+            contents.append(content.encode('utf-8'))
+        return contents
 
-baseURL = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseURL,1)
-#bdtb.getPage(1)
-#bdtb.getTitle()
-#bdtb.getPageNum()
-bdtb.getContent(bdtb.getPage(1))
+    def setFileTitle(self, title):
+        # 如果标题不是为None，即成功获取到标题
+        if title is not None:
+            self.file = open(title + ".txt", "w+")
+        else:
+            self.file = open(self.defaultTitle + ".txt", "w+")
+
+    def writeData(self, contents):
+        # 向文件写入每一楼的信息
+        for item in contents:
+            if self.floorTag == '1':
+                # 楼之间的分隔符
+                floorLine = "\n" + str(
+                    self.floor) + u"-----------------------------------------------------------------------------------------\n"
+                self.file.write(floorLine)
+            self.file.write(item)
+            self.floor += 1
+
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getPageNum(indexPage)
+        title = self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if pageNum == None:
+            print "URL已失效，请重试"
+            return
+        try:
+            print "该帖子共有" + str(pageNum) + "页"
+            for i in range(1, int(pageNum) + 1):
+                print "正在写入第" + str(i) + "页数据"
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+        # 出现写入异常
+        except IOError, e:
+            print "写入异常，原因" + e.message
+        finally:
+            print "写入任务完成"
+
+print u"请输入帖子代号"
+baseURL = 'http://tieba.baidu.com/p/' + str(raw_input(u'http://tieba.baidu.com/p/'))
+seeLZ = raw_input("是否只获取楼主发言，是输入1，否输入0\n")
+floorTag = raw_input("是否写入楼层信息，是输入1，否输入0\n")
+bdtb = BDTB(baseURL,seeLZ,floorTag)
+bdtb.start()
