0ec67181172534fb634ccefc85ebb7306e16729e
SoIomon <770589196@qq.com>
2017-04-28 23:54:07 +0800

    Day_4
    在去除内容杂物标签上测试成功，并加入楼层计数。怠惰了一下下。）

--- baidutieba_00.py
+++ baidutieba_00.py
@@ -78,11 +78,14 @@ class BDTB:
     def getContent(self, page):
         pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
         items = re.findall(pattern, page)
+        floor = 1
         #for item in items:
         #    print item
-        print self.tool.replace(items[1])
+        #print self.tool.replace(items[1])
         for item in items:
-            print item
+            print floor, u"楼------------------------------------------------------------------------------------------------------------------------------------\n"
+            print self.tool.replace(item)
+            floor += 1
 
 baseURL = 'http://tieba.baidu.com/p/3138733512'
 bdtb = BDTB(baseURL,1)
