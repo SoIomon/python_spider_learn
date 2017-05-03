61b70dbe4c0667c50f32489a6bbe372d0fb90a27
SoIomon <770589196@qq.com>
2017-05-03 21:09:44 +0800

    Day_7
    自动链接校园网。第一步，实现自动登录成功。

--- autolink.py
+++ autolink.py
@@ -4,22 +4,23 @@ import urllib2
 import urllib
 
 url = 'http://192.168.168.168/0.htm'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'
-'''headers = {
-'Connection': 'keep-alive',
-'Upgrade-Insecure-Requests':'keep-alive',
-'User-Agent':user_agent ,
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-'Referer': 'http://192.168.168.168/0.htm',
-'Accept-Encoding':'gzip, deflate, sdch',
-'Accept-Language': 'zh-CN,zh;q=0.8'}'''
-headers ={
-'User-Agent':user_agent
+
+headers = {
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests':'keep-alive',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36' ,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Referer': 'http://192.168.168.168/0.htm',
+    'Accept-Encoding':'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8'
 }
 data = {
-'username':'101001972000800',
-'password':'252838'
+    'DDDDD':'101001972000800',
+    'upass':'252838',
+    '0MKKey':'(unable to decode value)',
+    'v6ip':''
 }
-request = urllib2.Request(url,data=urllib.urlencode(data),headers=headers)
+post_data = urllib.urlencode(data)
+request = urllib2.Request(url,post_data,headers=headers)
 response = urllib2.urlopen(request)
-print response.read()
+print response.read().decode('GB2312')
